e6f8293a1ba64f3802d6b2e78aa1f6994847940a
Robert <rwallhead@gmail.com>
2021-11-13 15:19:53 +1100

    Fixed up side stl generation

--- print/build_stls.bat
+++ print/build_stls.bat
@@ -3,9 +3,9 @@ echo $fn = 100^; box_size = 100^; rotate([0, 180, 0]) cam()^; >> cam.scad
 "C:\Program Files\OpenSCAD\openscad.com" -o cam.stl cam.scad
 
 echo include ^<../magicbox.scad^>^; > side_with_servo_mount_slot.scad
-echo $fn = 100^; box_size = 100^; rotate([-90, 0, 0]) side_with_servo_mount_slot();^; >> side_with_servo_mount_slot.scad
+echo $fn = 100^; box_size = 100^; side_with_servo_mount_slot()^; >> side_with_servo_mount_slot.scad
 "C:\Program Files\OpenSCAD\openscad.com" -o side_with_servo_mount_slot.stl side_with_servo_mount_slot.scad
 
 echo include ^<../magicbox.scad^>^; > side_with_servo_rear_mount.scad
-echo $fn = 100^; box_size = 100^; rotate([90, 0, 0]) side_with_servo_rear_mount()^; >> side_with_servo_rear_mount.scad
+echo $fn = 100^; box_size = 100^; side_with_servo_rear_mount()^; >> side_with_servo_rear_mount.scad
 "C:\Program Files\OpenSCAD\openscad.com" -o side_with_servo_rear_mount.stl side_with_servo_rear_mount.scad
